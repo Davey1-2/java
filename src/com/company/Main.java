3cd3cfe91ae25dcfaf5ac2037e8202657546a70c
Davey1-2 <david.barta2014gmail.com>
2020-11-10 13:59:59 +0100

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] numbers = {10};
+        int[] tasks = {10};
+        int choose;
+        int managment;
+        String update;
+        String create;
 
-        for (int i = 0; i < numbers.length; i++) {
+        int i;
+
+
+        while (i < numbers.length){
+
             System.out.println("--------------------------------------------------");
             System.out.println("You can add up to 10 tasks");
             System.out.println("You currently have " + numbers[i] + "/10");
@@ -17,7 +26,6 @@ public class Main {
             System.out.println("2) Exit");
 
             int option = sc.nextInt();
-
             if (option == 1) {
                 System.out.println("--------------------------------------------------");
                 System.out.println("Tasks managment");
@@ -27,26 +35,27 @@ public class Main {
                 System.out.println("3) Destroy task");
                 System.out.println("4) Back to main menu");
 
-                int managment = sc.nextInt();
+                managment = sc.nextInt();
 
                 if (managment == 1){
-                    String create = sc.nextLine();
-                }
+                        System.out.println("Enter the name of the new task");
+                        create[i] = sc.nextLine();
+                        i++;
+                    }
+
 
                 if (managment == 2){
 
+                    System.out.println("Which task do you want to update?");
+                    choose = sc.nextInt();
+                    System.out.println("Enter your newest task");
+                    update[choose] = sc.nextLine();
+
+                    // jsem desperate nic nefunguje :(
+                    
                 }
 
-
-                
             }
-
-            else {
-                break;
-            }
-
-
         }
-
     }
 }
